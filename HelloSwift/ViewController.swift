10ba8e9d12df57d506d6f8397d3c42dac67d1fca
Josh Bruce <josh@surftrack.co.uk>
2015-09-29 11:45:01 +0100

    Source code from the HelloSwift tutorial on Swiftcasts

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Hello Swiftcasts!"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
